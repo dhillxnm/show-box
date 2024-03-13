2dcf5bebfb69d41ecb055708a4cb7f7ca33d001f
Adogamhe Aleogho Martins(Jnr.) Andre <martinsadogamhe9@gmail.com>
2024-03-12 23:03:52 -0500

    finished the nav sign-in function

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,93 +1,30 @@
 'use strict';
 
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    
-    // Here you can add your login validation logic
-    if (email && password) {
-        // Validation logic goes here
-        console.log('Login successful');
-    } else {
-        console.log('Invalid email or password');
+//  *** Global constants  ***
+const headerSigninbutton = document.querySelector('.header-button');
+
+
+//  *** Signin button listener function
+let signinDialog = false;
+headerSigninbutton.addEventListener('click', function() {
+    if (!signinDialog){
+        styleTag.innerHTML = `
+        .sign-in-menu {
+          display: none;
+        }
+        .secondary-hero-text {
+          display: none;
+        }
+        .dialog-container {
+          display: block;
+        }
+        `;
+        headTag.appendChild(styleTag);
+        signinDialog = true;
+    } 
+    else{
+        styleTag.innerHTML = ``;
+        signinDialog = false;
     }
 });
 
-// Utility function
-function onEvent(event, selector, callback) {
-  return selector.addEventListener(event, callback);
-}
-
-function getElement(selector, parent = document) {
-  return parent.getElementById(selector);
-}
-
-function select(selector, parent = document) {
-  return parent.querySelector(selector);
-}
-
-function selectAll(selector, parent = document) {
-  return [...parent.querySelectorAll(selector)];
-}
-
-function print(arg) {
-  console.log(arg);
-}
-
-// main code
-const contact = select('#contact');
-const dialog = select('#contact-frame');
-const overlay = select('#overlay');
-const alert = select('#alert');
-const fullName = select('#name');
-const email = select('#email');
-const message = select('#message');
-const button = select('#contact-btn');
-let isVisible = false;
-
-function clearForm() {
-  fullName.value = '';
-  email.value = '';
-  message.value = '';
-}
-
-function showDialog() {
-  dialog.classList.add('isvisible');
-  overlay.classList.add('isvisible');
-  isVisible = true;
-}
-
-function hideDialog() {
-  dialog.classList.remove('isvisible');
-  overlay.classList.remove('isvisible');
-  isVisible = false;
-}
-
-onEvent('load', window, function() {
-  clearForm();
-});
-
-onEvent('click', contact, function() {
-  showDialog()
-});
-
-onEvent('click', overlay, function() {
-  if (isVisible) hideDialog();
-});
-
-onEvent('keyup', document, function(event) {
-  if (isVisible && event.key === 'Escape') {
-    hideDialog();
-  }
-});
-
-onEvent('click', button, function() {
-  alert.classList.add('isvisible');
-  hideDialog();
-  setTimeout(function() {
-    alert.classList.remove('isvisible');
-    clearForm();
-  }, 5500);
-});
