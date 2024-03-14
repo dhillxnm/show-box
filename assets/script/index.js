c2e99e3e65042dbc5423fdec6f2a4051e369f422
Adogamhe Aleogho Martins(Jnr.) Andre <martinsadogamhe9@gmail.com>
2024-03-14 12:54:37 -0500

    added transision to the hero text

--- assets/script/index.js
+++ assets/script/index.js
@@ -28,8 +28,8 @@ const overlay = select('#overlay');
 const alert = select('#alert');
 const fullName = select('#name');
 const email = select('#email');
-const message = select('#message');
 const button = select('#contact-btn');
+const message = document.getElementById('message');
 let isVisible = false;
 
 function clearForm() {
@@ -75,4 +75,13 @@ onEvent('click', button, function() {
     alert.classList.remove('isvisible');
     clearForm();
   }, 5500);
+});
+
+// Function that displays hero text on page load
+onEvent('load', window, function() {
+    message.classList.toggle('visible');
+
+    this.innerText = message.classlist.contains('visible')?
+        'Hide message' : 'Show message'
+
 });
