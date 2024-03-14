aa1e110b65359693665ed56f48fb9f73e67f61b4
Adogamhe Aleogho Martins(Jnr.) Andre <martinsadogamhe9@gmail.com>
2024-03-13 21:03:46 -0500

    added basic utility functions to index.js

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,32 +1,22 @@
 'use strict';
 
-//  *** Global constants  ***
-const headerSigninbutton = document.querySelector('.header-button');
-const getStartedinput = document.querySelector('.landing-email-input');
-const getStartedbutton = document.querySelector('.landing-email-submit');
-const headTag = document.getElementsByTagName('head')[0];
-const styleTag = document.createElement("style");
+// Utility function
+function onEvent(event, selector, callback) {
+  return selector.addEventListener(event, callback);
+}
 
-let signinDialog = false;
-headerSigninbutton.addEventListener('click', function() {
-    if (!signinDialog){
-        styleTag.innerHTML = `
-        .sign-in-menu {
-          display: none;
-        }
-        .secondary-hero-text {
-          display: none;
-        }
-        .dialog-container {
-          display: block;
-        }
-        `;
-        headTag.appendChild(styleTag);
-        signinDialog = true;
-    } 
-    else{
-        styleTag.innerHTML = ``;
-        signinDialog = false;
-    }
-});
-  
+function getElement(selector, parent = document) {
+  return parent.getElementById(selector);
+}
+
+function select(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+function selectAll(selector, parent = document) {
+  return [...parent.querySelectorAll(selector)];
+}
+
+function print(arg) {
+  console.log(arg);
+}
